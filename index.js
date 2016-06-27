6d398c7e26379d42f0fad65f2199578ac6d04559
Sam <sam@Sams-MacBook-Pro.local>
2016-06-27 16:12:57 +0100

    [SW] Test Case Skeleton
    Signed-off-by: Sam <sam@Sams-MacBook-Pro.local>

---
+++ index.js
@@ -0,0 +1,52 @@
+// credit: https://github.com/sole/test_cases/
+
+window.addEventListener('load', function() {
+
+    var request = new XMLHttpRequest();
+    request.open('get', 'tests.json', true);
+    request.responseType = 'json';
+    request.addEventListener('load', onTestsLoaded);
+    request.addEventListener('error', onTestsLoadError);
+    request.send();
+
+    function tr(test) {
+        var title = test[0],
+            desc = test[1];
+        var row = document.createElement('tr');
+        var link = document.createElement('a');
+        link.href = title;
+        link.textContent = title;
+
+        var linkTD = document.createElement('td');
+        linkTD.appendChild(link);
+
+        var descTD = document.createElement('td');
+        descTD.textContent = desc;
+
+        row.appendChild(linkTD);
+        row.appendChild(descTD);
+        return row;
+    }
+
+    function appendTr(tr) {
+        document.getElementById('tBody').appendChild(tr);
+    }
+
+    function onTestsLoaded(e) {
+        var response = this.response;
+        
+        if(!response || !(response.tests)) {
+            onTestsLoadError(new Error('No tests found'));
+            return;
+        }
+
+        var list = document.createElement('dl');
+        document.body.appendChild(list);
+        response.tests.map(tr).forEach(appendTr);
+                
+    }
+
+    function onTestsLoadError(e) {
+    }
+
+});
